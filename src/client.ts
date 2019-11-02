891eeeb60124074dbfa81b7a71902a2c306bfa86
Wei Zhu <yesmeck@gmail.com>
2019-11-03 06:47:56 +0800

    finish fs

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { BASE_URL } from './url';
-import { SimpleSandbox, Sandbox } from './type';
+import { SimpleSandbox, Sandbox, Directory, Module } from './type';
 
 function getData(res: AxiosResponse) {
   return res.data.data;
@@ -33,5 +33,39 @@ export default class Client {
   async fetchSandbox(id: string): Promise<Sandbox> {
     const res = await this.request.get(`/sandboxes/${id}`);
     return getData(res);
+  }
+
+  async createModule(id: string, module: any): Promise<Module> {
+    const res = await this.request.post(`/sandboxes/${id}/modules`, {
+      module,
+    });
+    return getData(res);
+  }
+
+  async updateModule(id: string, moduleId: string, module: any) {
+    return await this.request.put(`/sandboxes/${id}/modules/${moduleId}`, {
+      module,
+    });
+  }
+
+  async deleteModule(id: string, moduleId: string) {
+    return await this.request.delete(`/sandboxes/${id}/modules/${moduleId}`);
+  }
+
+  async createDirectory(id: string, directory: any): Promise<Directory> {
+    const res = await this.request.post(`/sandboxes/${id}/directories`, {
+      directory,
+    });
+    return getData(res);
+  }
+
+  async deleteDirectory(id: string, dirId: string) {
+    return await this.request.delete(`/sandboxes/${id}/directories/${dirId}`);
+  }
+
+  async updateDirectory(id: string, dirId: string, directory: any) {
+    return await this.request.put(`/sandboxes/${id}/directories/${dirId}`, {
+      directory,
+    });
   }
 }
--- src/fs.ts
+++ src/fs.ts
@@ -1,9 +1,10 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 import Client from './client';
 import { Sandbox, Directory, Module } from './type';
 
 function isModule(file: any): file is Module {
-  return !!file.code;
+  return 'code' in file;
 }
 
 export default class FS implements vscode.FileSystemProvider {
@@ -22,6 +23,9 @@ export default class FS implements vscode.FileSystemProvider {
 
   async stat(uri: vscode.Uri) {
     const file = await this.lookup(uri);
+    if (!file) {
+      throw vscode.FileSystemError.FileNotFound(uri.path);
+    }
     return {
       ctime: new Date(file.updated_at).getTime(),
       mtime: new Date(file.inserted_at).getTime(),
@@ -32,6 +36,9 @@ export default class FS implements vscode.FileSystemProvider {
 
   async readDirectory(uri: vscode.Uri) {
     const dir = await this.lookup(uri);
+    if (!dir) {
+      throw vscode.FileSystemError.FileNotFound(uri.path);
+    }
     const result: [string, vscode.FileType][] = [];
     this.sandbox!.directories.forEach(d => {
       if (d.directory_shortid === dir.shortid) {
@@ -46,36 +53,85 @@ export default class FS implements vscode.FileSystemProvider {
     return result;
   }
 
-  createDirectory(uri: vscode.Uri): void | Thenable<void> {
-    throw new Error(`createDirectory not implemented.`);
+  async createDirectory(uri: vscode.Uri) {
+    const basename = path.basename(uri.path);
+    const dirname = uri.with({ path: path.dirname(uri.path) });
+    const parent = await this.lookup(dirname);
+    if (!parent) {
+      throw vscode.FileSystemError.FileNotFound(dirname.path);
+    }
+    const directory = await this.client.createDirectory(uri.authority, {
+      title: basename,
+      directory_shortid: parent.shortid,
+    });
+    this.sandbox!.directories.push(directory);
   }
 
   async readFile(uri: vscode.Uri) {
-    console.log(uri);
     const file = (await this.lookup(uri)) as Module;
-    console.log(file);
     return Buffer.from(file.code, 'utf8');
   }
 
-  writeFile(
-    uri: vscode.Uri,
-    content: Uint8Array,
-    options: { create: boolean; overwrite: boolean },
-  ): void | Thenable<void> {
-    throw new Error(`writeFile not implemented.`);
+  async writeFile(uri: vscode.Uri, content: Uint8Array) {
+    console.log('writeFile', uri);
+    const file = await this.lookup(uri);
+    if (file) {
+      await this.client.updateModule(uri.authority, file.shortid!, { code: content.toString() });
+    } else {
+      const basename = path.basename(uri.path);
+      const dirname = uri.with({ path: path.dirname(uri.path) });
+      const parent = await this.lookup(dirname);
+      if (!parent) {
+        throw vscode.FileSystemError.FileNotFound(dirname.path);
+      }
+      const module = await this.client.createModule(uri.authority, {
+        code: '',
+        directory_shortid: parent.shortid,
+        is_binary: false,
+        title: basename,
+      });
+      module.code = '';
+      this.sandbox!.modules.push(module);
+    }
   }
 
-  delete(uri: vscode.Uri, options: { recursive: boolean }): void | Thenable<void> {
-    throw new Error(`delete not implemented.`);
+  async delete(uri: vscode.Uri, options: { recursive: boolean }) {
+    const file = await this.lookup(uri);
+    if (!file) {
+      throw vscode.FileSystemError.FileNotFound(uri.path);
+    }
+    if (isModule(file)) {
+      this.sandbox!.modules.splice(this.sandbox!.modules.indexOf(file), 1);
+      await this.client.deleteModule(uri.authority, file.shortid!);
+    } else {
+      this.sandbox!.directories.splice(this.sandbox!.directories.indexOf(file), 1);
+      await this.client.deleteDirectory(uri.authority, file.shortid!);
+    }
   }
 
-  rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean }): void | Thenable<void> {
-    throw new Error(`renam not implemented.`);
+  async rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean }) {
+    const file = await this.lookup(oldUri);
+    if (!file) {
+      throw vscode.FileSystemError.FileNotFound(oldUri.path);
+    }
+    const basename = path.basename(newUri.path);
+    file.title = basename;
+    if (isModule(file)) {
+      await this.client.updateModule(oldUri.authority, file.shortid!, { title: basename });
+    } else {
+      await this.client.updateDirectory(oldUri.authority, file.shortid!, { title: basename });
+    }
   }
 
-  async lookup(uri: vscode.Uri): Promise<Directory | Module> {
+  async lookup(uri: vscode.Uri): Promise<Directory | Module | undefined> {
     if (!this.sandbox) {
-      this.sandbox = await this.client.fetchSandbox(uri.authority);
+      this.sandbox = await vscode.window.withProgress(
+        {
+          title: 'Loading sandbox...',
+          location: vscode.ProgressLocation.Notification,
+        },
+        () => this.client.fetchSandbox(uri.authority),
+      );
     }
     if (uri.path === '/') {
       return {
@@ -90,15 +146,15 @@ export default class FS implements vscode.FileSystemProvider {
     }
     const files = uri.path.replace(/^\//, '').split('/');
     const findFile = (name: string, dirId: string | null) => {
-        const module = this.sandbox!.modules.find(m => m.title === name && m.directory_shortid === dirId);
-        if (module) {
-          return module;
-        }
-        const dir = this.sandbox!.directories.find(m => m.title === name && m.directory_shortid === dirId);
-        if (dir) {
-          return dir;
-        }
-    }
+      const module = this.sandbox!.modules.find(m => m.title === name && m.directory_shortid === dirId);
+      if (module) {
+        return module;
+      }
+      const dir = this.sandbox!.directories.find(m => m.title === name && m.directory_shortid === dirId);
+      if (dir) {
+        return dir;
+      }
+    };
 
     let file: Directory | Module | undefined;
     let folder = files.shift();
@@ -110,7 +166,5 @@ export default class FS implements vscode.FileSystemProvider {
     if (file) {
       return file;
     }
-
-    throw vscode.FileSystemError.FileNotFound(uri.path);
   }
 }
